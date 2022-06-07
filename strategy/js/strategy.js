2f389442abdf0571e761fe115b1a69071c07220b
Irving Becerra <irving.becerra121@gmail.com>
2022-06-07 08:27:00 -0500

    Strategy caso practico

--- strategy/js/strategy.js
+++ strategy/js/strategy.js
@@ -49,18 +49,88 @@ class ForeignSalesStrategy{
 
 }
 
-const regularSale = new RegularSaleStrategy(0.16);
-const discountSale = new DiscountSaleStrategy(0.16, 3)
-const foreignSale = new ForeignSalesStrategy()
+// const regularSale = new RegularSaleStrategy(0.16);
+// const discountSale = new DiscountSaleStrategy(0.16, 3)
+// const foreignSale = new ForeignSalesStrategy()
 
-const sale = new SaleContext(regularSale);
+// const sale = new SaleContext(regularSale);
+// console.log(sale.calaculate(10))
+// sale.setStrategy(discountSale)
+// console.log(sale.calaculate(10))
+// sale.setStrategy(foreignSale)
+// console.log(sale.calaculate(10))
+// Explicacion practica
 
-console.log(sale.calaculate(10))
+const data = [
+    {
+        name: "El camino de los Reyes",
+        autor: "Brandon Sanderson",
+        saga: "El archivo de las tormentas"
+    },
+    {
+        name: "Harry Potter y la piedra Filosofal",
+        autor: "JK Rowling",
+        saga: "Harry Potter"
+    },
+    {
+        name: "La rueda del tiempo: El ojo del mundo",
+        autor: "Robert Jordan",
+        saga: "La rueda del tiempo"
+    }
+]
 
-sale.setStrategy(discountSale)
+class InfoContext{
 
-console.log(sale.calaculate(10))
+    constructor(strategy, data, element){
+        this.setStrategy(strategy)
+        this.data = data
+        this.element = element
+    }
 
-sale.setStrategy(foreignSale)
+    setStrategy(strategy){
+        this.strategy = strategy
+    }
 
-console.log(sale.calaculate(10))
+    show(){
+        this.strategy.show(this.data, this.element)
+    }
+}
+
+class ListStrategy{
+    show(data, element){
+        element.innerHTML = data.reduce((ac, libro)=>{
+            return ac + `<div>
+            <h2>${libro.name}</h2>
+            <p>${libro.autor}</p>
+        </div>
+        <hr>`
+        }, "")
+    }
+}
+
+
+class DetailedListStrategy{
+    show(data, element){
+        element.innerHTML = data.reduce((ac, libro)=>{
+            return ac + `<div>
+            <h2>${libro.name}</h2>
+            <p>${libro.autor}</p>
+            <p>${libro.saga}</p>
+        </div>
+        <hr>`
+        }, "")
+    }
+}
+
+const strategies = [
+    new ListStrategy(),
+    new DetailedListStrategy()
+]
+const info = new InfoContext(new ListStrategy(), data, content)
+info.show()
+
+slcOptions.addEventListener("change", (event)=> {
+    const op = event.target.value
+    info.setStrategy(strategies[op])
+    info.show()
+})
